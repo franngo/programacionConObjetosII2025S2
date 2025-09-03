b76d11aa7909f304df7fe6bb9a5765b5c4ad2844
Fran <155672163+franngo@users.noreply.github.com>
2025-09-03 11:58:31 -0300

    avance en la implementación en Java de TP 2

--- tp2-UML/implementacion-en-Java/src/empresa/Empresa.java
+++ tp2-UML/implementacion-en-Java/src/empresa/Empresa.java
@@ -1,5 +1,35 @@
 package empresa;
 
-public class Empresa {
+import empleado.*;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Empresa {
+	private String name;
+	private String cuil;
+	private List<Empleado> empleados;
+	private List<ReciboDeHaberes> recibosHaberes;
+	
+	public Empresa(String name, String cuil, List<Empleado> empleados) {
+		this.name = name;
+		this.cuil = cuil;
+		this.empleados = empleados;
+		this.recibosHaberes = new ArrayList<ReciboDeHaberes>();
+	}
+	
+	public void realizarLiquidacionSueldos() {
+		
+	}
+	
+	public float totalSueldosNetos() {
+		return 1;
+	}
+	
+	public float totalSueldosBrutos() {
+		return 1;
+	}
+	
+	public float retenciones() {
+		return 1;
+	}
 }
--- tp2-UML/implementacion-en-Java/src/empresa/ReciboDeHaberes.java
+++ tp2-UML/implementacion-en-Java/src/empresa/ReciboDeHaberes.java
@@ -1,5 +1,22 @@
 package empresa;
 
-public class ReciboDeHaberes {
+import java.util.Date;
 
+import empleado.*;
+
+public class ReciboDeHaberes {
+	private String direccion;
+	private Date fechaEmision;
+	private float sueldoBruto;
+	private float sueldoNeto;
+	private String desgloceConceptos;
+	
+	public ReciboDeHaberes(String direccion, Date fechaEmision, float sueldoBruto, float sueldoNeto, 
+			String desgloceConceptos) {
+		this.direccion = direccion;
+		this.fechaEmision = fechaEmision;
+		this.sueldoBruto = sueldoBruto;
+		this.sueldoNeto = sueldoNeto;
+		this.desgloceConceptos = desgloceConceptos;
+	}
 }
